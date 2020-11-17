95ad0bc5835eb850d9af65d5d54facaec78722b8
SNT parintele Damaschin <mihai.tirtara@gmail.com>
2020-11-17 22:59:14 +0100

    Client test connection

---
+++ Client/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            // Declare variables
+            string uirWebAPI, exceptionMessage, webResponse;
+
+            // Set the UIR endpoint link. It should go to the application config file 
+            uirWebAPI = "http://localhost:5000/api/v1.0/csharp_python_restfulapi_json";
+            exceptionMessage = string.Empty;
+
+            // Get web response by calling the CSharpPythonRestfulApiSimpleTest() method
+            webResponse = CSharpPythonRestfulApiSimpleTest(uirWebAPI, out exceptionMessage);
+
+            if (string.IsNullOrEmpty(exceptionMessage))
+            {
+                // No errors occurred. Write the string web response     
+                Console.WriteLine(webResponse.ToString());
+            }
+            else
+            {
+                // An error occurred. Write the exception message
+                Console.WriteLine(exceptionMessage);
+            }
+        }
+
+        public static string CSharpPythonRestfulApiSimpleTest(string uirWebAPI, out string exceptionMessage)
+        {
+            exceptionMessage = string.Empty;
+            string webResponse = string.Empty;
+            try
+            {
+                Uri uri = new Uri(uirWebAPI);
+                WebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(uri);
+                httpWebRequest.ContentType = "application/json";
+                httpWebRequest.Method = "POST";
+                using (StreamWriter streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+                {
+                    // Build employee test JSON objec
+                    dynamic employee = new JObject();
+                    employee.username = "theUserName";
+                    employee.password = "thePassword";
+                    streamWriter.Write(employee.ToString());
+                }
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    webResponse = streamReader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                exceptionMessage = $"An error occurred. {ex.Message}";
+            }
+            return webResponse;
+        }
+    }
+}
--- Server/server.py
+++ Server/server.py
@@ -1,9 +1,30 @@
 # import flask microframework library
 from flask import Flask
+from flask import request
+from flask import jsonify
+import json 
+import sys
+
  
 # initialize the flask application
 app = Flask(__name__)
- 
+@app.route("/api/v1.0/csharp_python_restfulapi_json", methods=["POST"])
+def csharp_python_restfulapi_json():
+    """
+    simple c# test to call python restful api web service
+    """
+    try:                
+#         get request json object
+        request_json = request.get_json()      
+#         convert to response json object 
+        response = jsonify(request_json)
+        response.status_code = 200  
+    except:
+        exception_message = sys.exc_info()[1]
+        response = json.dumps({"content":exception_message})
+        response.status_code = 400
+    return response
+
 if __name__ == "__main__":
 #     run flask application in debug mode
     app.run(debug=True)
