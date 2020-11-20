4f51ceb98c7c2b0f99174a8ee8107af2c5bf38d3
SNT parintele Damaschin <mihai.tirtara@gmail.com>
2020-11-20 11:51:00 +0100

    rest interface added

---
+++ Client/src/REST/Restfull.cs
@@ -0,0 +1,18 @@
+namespace RestfullApi
+{
+    public class Restfull : IRESTfull
+    {
+       public  string RestfullSimpleTest(string url,  string exceptionMessage)
+        {
+            return null;
+        }
+        public string RestfulImageClassificationBase64(string url, string imagePathName,  string exceptionMessage)
+        {
+            return null;
+        }
+        public string RestSharpImageClassificationBase64(string url, string imagePathName,  string exceptionMessage)
+        {
+            return null;
+        }
+    }
+}
