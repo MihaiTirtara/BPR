7a341dc32004f80d229ecb60718859a2802cb294
SNT parintele Damaschin <mihai.tirtara@gmail.com>
2020-12-01 21:52:35 +0100

    interface added

---
+++ Server/restfullInterface.py
@@ -0,0 +1,12 @@
+from inteface import Interface
+
+class RestfullInterface(Interface):
+    def downloadFile(file):
+        pass
+    def imageToText(image):
+        pass
+    def vectorizeObject(ocrText):
+        pass
+    def imageClassifier(object):
+        pass
+    
