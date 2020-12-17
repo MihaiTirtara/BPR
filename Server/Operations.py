d16bb5f12eb17f18668cad246cd93ca1acc66b80
Erika Szasz <ems@nobly.dk>
2020-12-17 19:33:52 +0100

    Server: refactored downloadFile and imageClassifier.

--- Server/Operations.py
+++ Server/Operations.py
@@ -7,89 +7,45 @@ import sys
 import time
 import traceback
 import json
-import numpy as np 
-import pandas as pd 
+import numpy as np
+import pandas as pd
 import io
-from cv2 import  cv2
+from cv2 import cv2
 import joblib
 from flask import jsonify
 import scipy.sparse.linalg
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+
 class Restfull():
 
-    def downloadFile(self, file):
-        print(file)
-        in_memory_file = io.BytesIO()
-        file.save(in_memory_file)
-        data = np.fromstring(in_memory_file.getvalue(), dtype=np.uint8)
-        color_image_flag = 1
-        img = cv2.imdecode(data, color_image_flag)
-        cv2.imwrite("uploads\\" + file.filename, img)
-        print("uploads\\" + file.filename)
-        return("uploads\\" + file.filename)
+    def downloadFile(self, document):
+        file_content = bytes(document["FileContent"])
+        np_array_encode = np.fromstring(file_content, np.uint8)
+        image = cv2.imdecode(np_array_encode, 1)
+        imagePath = "uploads\\test.png"
+        cv2.imwrite(imagePath, image)
+        return imagePath
 
-    def imageToText(self,filename,tesseractPath):
+    def imageToText(self, filename, tesseractPath):
         pytesseract.pytesseract.tesseract_cmd = tesseractPath
         ocrText = pytesseract.image_to_string(filename)
         print(ocrText)
         return ocrText
-    
-    def preprocessText(self,ocrText, vectorizerPath):
+
+    def preprocessText(self, ocrText, vectorizerPath):
         vectorizer = joblib.load(vectorizerPath)
         textArray = [ocrText]
         vectorText = vectorizer.transform(textArray)
-        vectorText = vectorText.reshape(1,-1)
+        vectorText = vectorText.reshape(1, -1)
         return vectorText
 
-    def imageClassifier(self,obj, modelPath):
+    def imageClassifier(self, obj, modelPath):
         types = ["Letter", "TooLazy"]
-        classifiermodel = joblib.load(modelPath)
-        try:    
-            #Prediction
+        try:
+            classifiermodel = joblib.load(modelPath)
+            # Prediction
             YPredict = classifiermodel.predict(obj)
-            return types[YPredict[0]]
-            if YPredict[0] == 0:
-                result = 'The document is a Letter'
-            elif YPredict[0] == 1:
-                result = 'The document is a Form'
-            elif YPredict[0] == 2:
-                result = 'The document is a E-mail'  
-            elif YPredict[0] == 3:
-                result = 'The document is a Handwriten'       
-            elif YPredict[0] == 4:
-                result = 'The document is a Advertisment'
-            elif YPredict[0] == 5:
-                result = 'The document is a Scientific Report'
-            elif YPredict[0] == 6:
-                result = 'The document is a Scientific Publication'
-            elif YPredict[0] == 7:
-                result = 'The document is a Specification'
-            elif YPredict[0] == 7:
-                result = 'The document is a Specification'
-            elif YPredict[0] == 8:
-                result = 'The document is a File Folder'
-            elif YPredict[0] == 9:
-                result = 'The document is a News Article'
-            elif YPredict[0] == 10:
-                result = 'The document is a Budget'
-            elif YPredict[0] == 11:
-                result = 'The document is a Invoice'
-            elif YPredict[0] == 12:
-                result = 'The document is a Presentation'
-            elif YPredict[0] == 13:
-                result = 'The document is a Questionaire'
-            elif YPredict[0] == 14:
-                result = 'The document is a Resume'
-            elif YPredict[0] == 15:
-                result = 'The document is a Memo'
-            else:
-                result = 'No document'                                
-            #Return response
-            response = jsonify(result)
-            response.status_code = 200
+            return {'groupName': types[YPredict[0]], 'error': None}
         except:
-            result = "An error occured"
-            response = jsonify(result)
-            response.status_code = 400
-        return response
+            return {'groupName': None, 'error': 'An error occurred while classifying'}
--- Server/server.py
+++ Server/server.py
@@ -2,7 +2,7 @@
 from flask import Flask, Response
 from flask import request
 from flask import jsonify
-import json 
+import json
 import sys
 import io
 import numpy as np
@@ -10,34 +10,27 @@ import base64
 from cv2 import cv2
 
 from Operations import Restfull
- 
+
 # initialize the flask application
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
 
 @app.route("/api/v1.0/documents/classification/automatic", methods=["POST"])
-def automaticallyClasifyDocuments(): 
-    #Trying to decode the image
+def automaticallyClassifyDocuments():
+    # Trying to decode the image
     autoClassificationsDocuments = request.json
     classificationResults = []
     for document in autoClassificationsDocuments:
-        file_content = bytes(document["FileContent"])
-        np_array_encode = np.fromstring(file_content, np.uint8)
-        image = cv2.imdecode(np_array_encode, 1)
-        imagePath = "uploads\\test.png"
-        cv2.imwrite(imagePath, image)
-        ocrText = rest.imageToText(imagePath,app.config['TESSCONFIG'])
-        processedText = rest.preprocessText(ocrText,app.config['VECTORIZER'])
-        response = rest.imageClassifier(processedText,app.config['MODEL'])
-        classificationResults.append({'groupName': response, 'error': None})
-
+        imagePath = rest.downloadFile(document)
+        ocrText = rest.imageToText(imagePath, app.config['TESSCONFIG'])
+        processedText = rest.preprocessText(ocrText, app.config['VECTORIZER'])
+        classificationResult = rest.imageClassifier(processedText, app.config['MODEL'])
+        classificationResults.append(classificationResult)
     return jsonify(classificationResults)
 
-    
-    #return response
 
-def manualyClassifyDocuments(documents):
+def manuallyClassifyDocuments(documents):
     print("Not implemented")
 
 
@@ -45,5 +38,5 @@ if __name__ == "__main__":
     global rest
     rest = Restfull()
     app.run(debug=True)
-#     run flask application in debug mode
+    #     run flask application in debug mode
     app.run(debug=True)
